0991ae0db0e38bb240ff60dc6c95b5efb291e100
Jay Imort <jayimort2005@gmail.com>
2022-06-26 19:46:34 +0200

    besser

--- client/src/DetailsPopUp.jsx
+++ client/src/DetailsPopUp.jsx
@@ -12,47 +12,9 @@ import { Box } from '@mui/material';
 export default function DetailsPopUp(props) {
     const { open, onClose, contract } = props;
 
-    const getIp2 = () => {
-        if(contract.ip2) {
-            return contract.ip2;
-        }
-        return "-";
-    }
-    const getIp3 = () => {
-        if(contract.ip3) {
-            return contract.ip3;
-        }
-        return "-";
-    }
-
-    const getNumFeature1 = () => {
-        if(contract.numFeature1) {
-            return contract.numFeature1;
-        }
-        return "-";
-    }
-    const getNumFeature2 = () => {
-        if(contract.numFeature2) {
-            return contract.numFeature2;
-        }
-        return "-";
-    }
-    const getNumFeature3 = () => {
-        if(contract.numFeature3) {
-            return contract.numFeature3;
-        }
-        return "-";
-    }
-
-    const getUser1 = () => {
-        if(contract.user1) {
-            return contract.user1;
-        }
-        return "-";
-    }
-    const getUser2 = () => {
-        if(contract.user2) {
-            return contract.user2;
+    const isEmpty = (x) => {
+        if(x) {
+            return x;
         }
         return "-";
     }
@@ -80,28 +42,28 @@ export default function DetailsPopUp(props) {
                             IP 1: {contract.contractIp1}
                         </Typography>
                         <Typography>
-                            IP 2: {getIp2()}
+                            IP 2: {isEmpty(contract.contractIp2)}
                         </Typography>
                         <Typography>
-                            IP 3: {getIp3()}
+                            IP 3: {isEmpty(contract.contractIp3)}
                         </Typography>
                         <Typography>
                             License Key: {contract.contractLicenseKey}
                         </Typography>
                         <Typography>
-                            Num Feature 1: {getNumFeature1()}
+                            Num Feature 1: {isEmpty(contract.contractNumFeature1)}
                         </Typography>
                         <Typography>
-                            Num Feature 2: {getNumFeature2()}
+                            Num Feature 2: {isEmpty(contract.contractNumFeature2)}
                         </Typography>
                         <Typography>
-                            Num Feature 3: {getNumFeature3()}
+                            Num Feature 3: {isEmpty(contract.contractNumFeature3)}
                         </Typography>
                         <Typography>
-                            User 1: {getUser1()}
+                            User 1: {isEmpty(contract.contractUser1)}
                         </Typography>
                         <Typography>
-                            User 2: {getUser2()}
+                            User 2: {isEmpty(contract.contractUser2)}
                         </Typography>
                         <Typography>
                             Version: {contract.contractVersion}
